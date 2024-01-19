bad76cb2d3ce00fb95f24a0d6faff23d7b8791d8
jadsoncerqueira <jadsoncerqueira97@gmail.com>
2024-01-19 14:02:56 -0300

    rota de usuarios

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -12,39 +12,47 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("user")]
 public class UserController : ControllerBase
-{    
+{
     public readonly IUserService _service;
-    
+
     public UserController(IUserService service)
     {
-        this._service = service;        
+        this._service = service;
     }
 
     // 6 - Sua aplicação deve ter o endpoint GET /user/:email
     [HttpGet("{email}", Name = "GetUser")]
     public IActionResult Get(string email)
-    {                
-        throw new NotImplementedException();
+    {
+        if (!_service.UserExists(email)) return NotFound();
+        var user = _service.GetUser(email);
+        return Ok(user);
     }
 
     // 7 - Sua aplicação deve ter o endpoint POST /user
     [HttpPost]
-    public IActionResult Create([FromBody]User user)
+    public IActionResult Create([FromBody] User user)
     {
-        throw new NotImplementedException();
+        _service.AddUser(user);
+        return CreatedAtRoute("GetUser", new { email = user.Email }, user);
     }
 
     // "8 - Sua aplicação deve ter o endpoint PUT /user
     [HttpPut("{email}")]
-    public IActionResult Update(string email, [FromBody]User user)
+    public IActionResult Update(string email, [FromBody] User user)
     {
-        throw new NotImplementedException();
+        if (email.ToLower() != user.Email.ToLower()) return BadRequest();
+        if (!_service.UserExists(email)) return NotFound();
+        _service.UpdateUser(user);
+        return Ok(user);
     }
 
-    // 9 - Sua aplicação deve ter o endpoint DEL /user
+    // 9 - Sua aplicação deve ter o endpoint DELETE /user
     [HttpDelete("{email}")]
     public IActionResult Delete(string email)
     {
-        throw new NotImplementedException();
-    } 
+        if (!_service.UserExists(email)) return NotFound();
+        _service.DeleteUser(email);
+        return NoContent();
+    }
 }
